c7297a070a2d0a3a458c5c04f8b859b7524760d7
Idir Ouhab Meskine <ouhab.idir@gmail.com>
2020-06-02 17:24:02 +0200

    add mix endpoint

--- constants.js
+++ constants.js
@@ -34,24 +34,66 @@ const eventSource = [
     "sensu",
 ];
 
+const metric = [
+    "cpu_utilisation",
+    "mem_percent_free",
+    "disk_space_free",
+    "net_bytes_dropped",
+    "throughput",
+    "response_time",
+];
+
+const status = [
+    "up",
+    "down",
+];
+
 const items = {
     dc,
     env,
     application,
     service,
-    eventSource
+    eventSource,
+    metric,
+    status
 };
-
 
 const getRandomItem = (itemName) => {
     if (itemName === 'iterator') {
         return Math.floor(Math.random() * 100) + 1
     }
-
     const item = items[itemName];
+
     return item[Math.floor(Math.random() * item.length)];
 };
 
+const hostNameGenerator = () => {
+    const elements = [
+        getRandomItem('dc'),
+        getRandomItem('env'),
+        getRandomItem('iterator'),
+    ];
+    return elements.join("-");
+};
+
+const getMix = () => {
+    const iterator = getRandomItem('iterator');
+    const dc = getRandomItem('dc');
+    const env = getRandomItem('env');
+    return {
+        dc: dc,
+        env: env,
+        hostname: `${dc}-${env}-${iterator}`,
+        application: getRandomItem('application'),
+        service: getRandomItem('service'),
+        eventSource: getRandomItem('eventSource'),
+        metric: getRandomItem('metric'),
+        status: getRandomItem('status'),
+    };
+};
+
 module.exports = {
-    getRandomItem
+    hostNameGenerator,
+    getRandomItem,
+    getMix
 };
--- index.js
+++ index.js
@@ -1,37 +1,55 @@
-require('newrelic');
-const {getRandomItem} = require('./constants');
-const express = require('express');
+require("newrelic");
+const { hostNameGenerator, getRandomItem, getMix } = require("./constants");
+const express = require("express");
 const app = express();
-const hostNameGenerator = () => {
-    const elements = [
-        getRandomItem('dc'),
-        getRandomItem('env'),
-        getRandomItem('iterator'),
-    ];
-    return elements.join("-");
-};
+
 const LIMIT = 100000;
-app.get('/api/hostname/:number', (req, res) => {
-    const amount = req.params.number > LIMIT ? LIMIT : req.params.number;
-    res.send({names: Array.from({length: amount}, () => hostNameGenerator())})
+app.get("/api/hostname/:number", (req, res) => {
+  const amount = req.params.number > LIMIT ? LIMIT : req.params.number;
+  res.send({ names: Array.from({ length: amount }, () => hostNameGenerator()) });
 });
 
-app.get('/api/application/:number', (req, res) => {
-    const amount = req.params.number > LIMIT ? LIMIT : req.params.number;
-    res.send({names: Array.from({length: amount}, () => getRandomItem("application"))})
+app.get("/api/application/:number", (req, res) => {
+  const amount = req.params.number > LIMIT ? LIMIT : req.params.number;
+  res.send({ names: Array.from({ length: amount }, () => getRandomItem("application")) });
 });
 
-app.get('/api/service/:number', (req, res) => {
-    const amount = req.params.number > LIMIT ? LIMIT : req.params.number;
-    res.send({names: Array.from({length: amount}, () => getRandomItem("service"))})
+app.get("/api/service/:number", (req, res) => {
+  const amount = req.params.number > LIMIT ? LIMIT : req.params.number;
+  res.send({ names: Array.from({ length: amount }, () => getRandomItem("service")) });
 });
 
-app.get('/api/event-source/:number', (req, res) => {
-    const amount = req.params.number > LIMIT ? LIMIT : req.params.number;
-    res.send({names: Array.from({length: amount}, () => getRandomItem("eventSource"))})
+app.get("/api/event-source/:number", (req, res) => {
+  const amount = req.params.number > LIMIT ? LIMIT : req.params.number;
+  res.send({ names: Array.from({ length: amount }, () => getRandomItem("eventSource")) });
+});
+
+app.get("/api/metric/:number", (req, res) => {
+  const amount = req.params.number > LIMIT ? LIMIT : req.params.number;
+  res.send({ names: Array.from({ length: amount }, () => getRandomItem("metric")) });
+});
+
+app.get("/api/status/:number", (req, res) => {
+  const amount = req.params.number > LIMIT ? LIMIT : req.params.number;
+  res.send({ names: Array.from({ length: amount }, () => getRandomItem("status")) });
+});
+
+app.get("/api/data-center/:number", (req, res) => {
+  const amount = req.params.number > LIMIT ? LIMIT : req.params.number;
+  res.send({ names: Array.from({ length: amount }, () => getRandomItem("dc")) });
+});
+
+app.get("/api/mix/:number", (req, res) => {
+  const amount = req.params.number > LIMIT ? LIMIT : req.params.number;
+  res.send({ data: Array.from({ length: amount }, () => getMix()) });
+});
+
+app.get("/api/environment/:number", (req, res) => {
+  const amount = req.params.number > LIMIT ? LIMIT : req.params.number;
+  res.send({ names: Array.from({ length: amount }, () => getRandomItem("env")) });
 });
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, function() {
-    console.log(`Running  on port ${PORT}`);
+app.listen(PORT, function () {
+  console.log(`Running  on port ${PORT}`);
 });
